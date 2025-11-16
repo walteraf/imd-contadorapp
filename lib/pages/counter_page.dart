10b32484eede1b8c1cb1f64cb4a0d51aa6959bce
Camila Braga <milabragalima@hotmail.com>
2025-11-15 21:23:13 -0300

    Implementação dos botões adicionais e responsividade

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -23,32 +23,70 @@ class CounterPage extends StatelessWidget {
           const SizedBox(height: 30),
           BigCounterCard(counter: appState.counter),
           const SizedBox(height: 30),
+          LayoutBuilder(builder: (context, constraints) {
+            bool isSmall = constraints.maxWidth < 600;
 
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: appState.decrement,
-                icon: const Icon(Icons.remove),
-                label: const Text('Decrementar'),
-              ),
-              const SizedBox(width: 15),
-              ElevatedButton.icon(
-                onPressed: appState.increment,
-                icon: const Icon(Icons.add),
-                label: const Text('Incrementar'),
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 15),
-
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: appState.subtractTwo,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.remove),
+                      SizedBox(width: 8),
+                      Text('2'),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                isSmall
+                    ? ElevatedButton(
+                        // Tela pequena
+                        onPressed: appState.decrement,
+                        child: const Icon(Icons.remove),
+                      )
+                    : ElevatedButton.icon(
+                        // Tela média a grande
+                        onPressed: appState.decrement,
+                        icon: const Icon(Icons.remove),
+                        label: const Text('Decrementar'),
+                      ),
+                const SizedBox(width: 15),
+                isSmall
+                    ? ElevatedButton(
+                        // Tela pequena
+                        onPressed: appState.increment,
+                        child: const Icon(Icons.add),
+                      )
+                    : ElevatedButton.icon(
+                        // Tela média a grande
+                        onPressed: appState.increment,
+                        icon: const Icon(Icons.add),
+                        label: const Text('Incrementar'),
+                      ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: appState.addTwo,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.add),
+                      SizedBox(width: 8),
+                      Text('2'),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
+          const SizedBox(height: 50),
           ElevatedButton.icon(
             onPressed: appState.reset,
             icon: const Icon(Icons.refresh),
             label: const Text('Zerar'),
           ),
-
           const Spacer(flex: 3),
         ],
       ),
